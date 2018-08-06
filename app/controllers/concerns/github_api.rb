e5ff0763987c0e6c195c5278d13d683f468db199
syathi <femto5401@gmail.com>
2018-08-06 23:24:07 +0900

    divide github api module

--- app/controllers/concerns/github_api.rb
+++ app/controllers/concerns/github_api.rb
@@ -2,12 +2,24 @@ require 'graphql/client'
 require 'graphql/client/http'
 
 module GitHub_API  
+  # スキーマ呼び出し
   HTTP = GraphQL::Client::HTTP.new("https://api.github.com/graphql") do
     def headers(context)
       {
         "Authorization": "Bearer #{ENV['GITHUB_API_TOKEN']}"
       }
     end
+  end
   Schema = GraphQL::Client.load_schema(HTTP)
-  Client = GraphQL::Client.new(Schema: Schema, execute: HTTP)
+  Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+
+  # サンプルクエリ(GraphQLのリポジトリ数)
+  Get_repository_count = GitHub_API::Client.parse <<-'GraphQL'
+    query { 
+			search(query: "GraphQL", type: REPOSITORY) {
+				 repositoryCount 
+			}
+		}
+  GraphQL
+  
 end
--- app/controllers/concerns/request_api.rb
+++ app/controllers/concerns/request_api.rb
@@ -2,33 +2,20 @@ require 'graphql/client'
 require 'graphql/client/http'
 require 'uri'
 require 'json'
-#GitHub GraphQL apiのサンプルリクエスト 
-module GitHub_API  
-		HTTP = GraphQL::Client::HTTP.new("https://api.github.com/graphql") do
-			def headers(context)
-				{
-					"Authorization" => "bearer #{ENV['GITHUB_API_TOKEN']}"
-				}
-			end
-		end
-    Schema = GraphQL::Client.load_schema(HTTP)
-    Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
-end
+require 'github_api'
+require 'api_query'
+include GitHub_API
+include Api_Query
 
+#GitHub GraphQL apiのサンプルリクエスト 
+#TODO: スキーマの読み込みはダンプファイルとかを使ってパフォーマンス改善する
 module Request_api
-	extend ActiveSupport::Concern
-	include GitHub_API
-	included do 
-		Query = GitHub_API::Client.parse <<-'GraphQL'
-			query { 
-				search(query: "GraphQL", type: REPOSITORY) {
-					 repositoryCount 
-			  }
-			}
-		GraphQL
-		def request_sample
-			res = GitHub_API::Client.query(Query)
-			query_result = "repository_count: #{res.data.search.repository_count}"
-		end
-	end
+  extend ActiveSupport::Concern
+  included do 
+    Query = GitHub_API::Get_repository_count
+    def request_sample
+      res = GitHub_API::Client.query(Query)
+      query_result = "repository_count: #{res.data.search.repository_count}"
+    end
+  end
 end
