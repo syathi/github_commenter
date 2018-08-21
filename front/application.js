d948af929b2d4037586bc7b4dea9f7308893f576
syathi <femto5401@gmail.com>
2018-08-21 23:38:30 +0900

    test button tag

--- front/application.js
+++ front/application.js
@@ -2,9 +2,18 @@ import React from "react";
 import { render } from "react-dom";
 
 class App extends React.Component {
-    render() {
-        return <p> hello React! </p>;
-    }
+  render() {
+    return (
+      <button 
+        onClick={this.requestSample}>
+        りろーど
+      </button>
+    );
+  }
+
+  requestSample(){
+    console.log("reload"); 
+  }
 }
 render(<App/>, document.getElementById("app"));
 console.log("hello");
